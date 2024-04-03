0a4e1fe4160e3deef2423a1a7faa044f016490af
nguyenthanhthanhtu229Vn <insec229@gmail.com>
2024-04-03 17:48:26 +0700

    fix

--- src/modules/checkout/Checkout.jsx
+++ src/modules/checkout/Checkout.jsx
@@ -5,14 +5,14 @@ import Subtotal from "./Subtotal";
 import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "../../hooks/useStateValue";
 const Checkout = () => {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
         <img src={Ad} alt="product-image" className="checkout__ad" />
 
         <div className="">
-          {/* <h3>Hello, {user?.email}</h3> */}
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">
             {basket?.length > 0
               ? "Your shopping Basket"
