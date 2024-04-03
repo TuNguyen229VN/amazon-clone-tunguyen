66a5d8e06b1ed677c9c270f98bbf864320d1ec76
nguyenthanhthanhtu229Vn <insec229@gmail.com>
2024-04-03 17:45:13 +0700

    f

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   dotenv: ".env.development",
-  "rewrites": [{ "source": "/(.*)", "destination": "/index.html" }]
-
+  rewrites: [{ source: "/(.*)", destination: "/index.html" }],
+  routes: [{ src: "/[^.]+", dest: "/", status: 200 }],
 });
