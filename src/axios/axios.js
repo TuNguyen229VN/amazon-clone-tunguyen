a486558260d2a6108547770310d12151533dd400
nguyenthanhthanhtu229Vn <insec229@gmail.com>
2024-04-08 22:03:18 +0700

    feat:fix api

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-b2027/us-central1/api",
+  baseURL: "http://localhost:3000/api",
 });
 
 export default instance;
